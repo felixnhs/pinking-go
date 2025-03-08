12a0e7d974cd8bc661d7b9f976e0532f21da5a39
Zedane <42522808+Zedane@users.noreply.github.com>
2025-03-08 14:43:57 +0100

    init: LynxJS App

---
+++ ui/webhost/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+import '@lynx-js/web-core';
+import '@lynx-js/web-elements/index.css';
+import '@lynx-js/web-core/index.css';
+
+const App = () => {
+  return (
+    <lynx-view 
+      style={{ height: '100vh', width: '100vw' }}
+      url='/main.web.bundle'></lynx-view>
+  );
+};
+
+export default App;
